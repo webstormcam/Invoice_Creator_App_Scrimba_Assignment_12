ce33e2bdb6d49901aa2f2e7cf9968dc44af89919
Cameron Chandler <webstormcam@gmail.com>
2023-02-05 21:05:20 -0500

    Button is linked to input

---
+++ script.js
@@ -0,0 +1,10 @@
+const arrayOfTasks = []
+
+document.getElementById('addTask').addEventListener('click',addTask)
+
+function addTask(){
+const taskName = document.getElementById('task').value
+console.log(taskName)
+}
+
+console.log('wajs')
