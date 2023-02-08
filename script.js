ab8d880a40b6c0e715381aff6b1c0d224087c3ff
Cameron Chandler <webstormcam@gmail.com>
2023-02-07 22:18:48 -0500

    Working on the bottom half of the invoice

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ document.getElementById('addTask').addEventListener('click',addTask)
 function addTask(){
 let taskName = document.getElementById('task')
 const taskPrice = document.getElementById('priceOption').value
+for(let task of arrayOfTasks){
+    if(task.name.toLowerCase()===taskName.value.toLowerCase()){
+        taskName.value=""
+        taskName.placeholder="Enter in a new task!"
+        return
+
+    }
+}
 if(taskName.value){
     function task(name,price){
         this.name = name
@@ -39,10 +47,12 @@ document.addEventListener('click',function(e){
 
 
 function renderTasks(){
-    const tasks = document.getElementById('taskOutput')
-    let output=""
+    const tasksPlusTotal = document.getElementById('taskOutput')
+    let taskOutput=""
+    let TotalAmountOutput=""
+    let total=0
     for(let i=0; i<arrayOfTasks.length; i++){
-output+=`<div class="listed-task">
+taskOutput+=`<div class="listed-task">
 <div class="task-title-and-remove">
 <p class="task-word-style">${arrayOfTasks[i].name}</p>
 <button class="remove-btn" data-item="${[i]}">Remove</button>
@@ -50,7 +60,15 @@ output+=`<div class="listed-task">
 <div class="task-price-style"><p><span class="grey-dollar">$</span>${arrayOfTasks[i].price}</p></div>
 </div>`
     }
-    tasks.innerHTML = output
+    TotalAmountOutput=`
+    
+    
+    
+    
+    
+    
+    `
+    tasksPlusTotal.innerHTML = taskOutput
 }
 
 
